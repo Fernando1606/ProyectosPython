1844fc45ac44bb58e1030558cad8392e7e6b529a
Fernando <fernandojaviersolanceflores@educarex.es>
2022-06-02 12:18:44 +0200

    Ejercicios basicos

---
+++ calculadoraTiempoViaje.py
@@ -0,0 +1,7 @@
+velocidad = int (input('Dime tu velocidad --> '))
+
+distancia = int (input('Dime la distancia al destino --> '))
+
+resultado = (distancia / velocidad)
+
+print ('Tiempo estimado: ' + str(resultado) + ' horas')
